994b578765477ae3733e49c9d0f852845d220610
Hendrius Félix <infos.hendrius@unidas.com.br>
2026-07-27 12:01:00 -0300

    refactor(runtime): adapt capture infrastructure ports

--- src/infrastructure/media/__init__.py
+++ src/infrastructure/media/__init__.py
@@ -1,5 +1,11 @@
 """Media adapters."""
 
+from .capture_process import SubprocessCaptureProcess
 from .legacy_adapter import LegacyMediaToolAdapter
+from .segment_buffer_repository import SegmentBufferRepository
 
-__all__ = ["LegacyMediaToolAdapter"]
+__all__ = [
+    "LegacyMediaToolAdapter",
+    "SegmentBufferRepository",
+    "SubprocessCaptureProcess",
+]
